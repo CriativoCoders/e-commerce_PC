d56b0d0f9448925853d0e3d378756f103285eb80
CriativoCoders <talitacristina13877@gmail.com>
2025-06-30 16:02:06 -0300

    new

---
+++ ecommerce/scripts/database.js
@@ -0,0 +1,212 @@
+export const database = [ /* "Banco de dados" de produtos comercializados no site */
+
+    /*NOVIDADES */
+
+    {
+        codigoProduto:1,
+        tituloProduto:"Kit Gamer Teclado Semi Mecanico Gamer Iluminado, Mouse Gamer Com Led USB 3200DPI, 7 Botões", 
+        preco: 250.00, 
+        descricao:"Kit teclado Gamer.",
+        imagemProduto: {
+            img1:"./images/products/prod01/img1.png",
+            img2:"./images/products/prod01/img2.png",
+            img3:"./images/products/prod01/img3.png",
+            img4:"./images/products/prod01/img4.png",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:2,
+        tituloProduto:"Kit Gamer Teclado Semi-mecânico + Mouse RGB 3200 Dpi + Mouse Pad - Compatível com Computador Notebook e Vídeogame", 
+        preco: 350.00, 
+        descricao:"Está procurando por periféricos de qualidade para suas longas horas de jogatina? então você acaba de encontrar! itens do kit:1x mouse 3200 dp1x teclado semi-mecânico1x headset (com conexão p3)1x mousepad 220x180x2mm (sortido).",
+        imagemProduto: {
+            img1:"./images/products/prod01/img2.png",
+            img2:"./images/products/prod01/img1.png",
+            img3:"./images/products/prod01/img3.png",
+            img4:"./images/products/prod01/img2.png",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:3,
+        tituloProduto:"Kit Teclado Semi Mecânico Anti-Ghosting e Mouse Gamer Com Iluminação RGB + Fone Profissional Com Fio", 
+        preco: 400.00, 
+        descricao:"Kit Teclado Semi Mecânico Anti-Ghosting e Mouse Gamer Com Iluminação RGB + Fone Profissional Com Fio.",
+        imagemProduto: {
+            img1:"./images/products/prod01/img3.png",
+            img2:"./images/products/prod01/img2.png",
+            img3:"./images/products/prod01/img3.png",
+            img4:"./images/products/prod01/img4.png",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:4,
+        tituloProduto:"kit Teclado Semi Mecânico Anti-Ghosting e Mouse Gamer com iluminação RGB + Fone sem fio ", 
+        preco: 600.00, 
+        descricao:"Kit de Teclados semi Mecânico Com Iluminação RGB + Fone Otimos para jogos de Alta Performace !!!.",
+        imagemProduto: {
+            img1:"./images/products/prod01/img4.png",
+            img2:"./images/products/prod01/img2.png",
+            img3:"./images/products/prod01/img3.png",
+            img4:"./images/products/prod01/img4.png",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+   
+    /**PROMOÇÕES */
+
+    {
+        codigoProduto:5,
+        tituloProduto:"Galax Gaming ‎Chair GC-02", 
+        preco: 2000.00,
+        descricao:".",
+        imagemProduto: {
+            img1:"./images/products/prod02/img2.png",
+            img2:"./images/products/prod02/img2.png",
+            img3:"./images/products/prod02/img3.png",
+            img4:"./images/products/prod02/img4.png",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true
+    },
+    {
+        codigoProduto:6,
+        tituloProduto:"Pc Gamer Jax / Ryzen 5 5600GT / 16GB DDR4 / NVME 480GB ", 
+        preco: 2692.00, 
+        descricao:"Acompanha: Cabo de força, caixas, manuais e acessórios das peças utilizadas na montagem. Garantia total: 12 meses (contra defeitos de fabricação), 3 meses referentes à garantia legal, nos termos do artigo 26, II, do Código de Defesa do Consumidor + 9 meses de garantia do fabricante..",
+        imagemProduto: {
+            img1:"./images/products/prod02/img3.png",
+            img2:"./images/products/prod02/img3.png",
+            img3:"./images/products/prod02/img4.png",
+            img4:"./images/products/prod02/img1.png",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true
+    },
+    {
+        codigoProduto:7,
+        tituloProduto:"Pc Gamer Haven / Ryzen 5 5500 / 16GB DDR4 / Radeon RX 6600 8GB / SSD 480GB ", 
+        preco: 3952.00, 
+        descricao:" Acompanha: Cabo de força, caixas, manuais e acessórios das peças utilizadas na montagem.Todas as máquinas vão com Windows 11 (TRIAL) Instalado.",
+        imagemProduto: {
+            img1:"./images/products/prod02/img4.png",
+            img2:"./images/products/prod02/img2.png",
+            img3:"./images/products/prod02/img3.png",
+            img4:"./images/products/prod02/img4.png",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:8,
+        tituloProduto:"Pc Gamer Screen / Ryzen 5 5600 / 16GB DDR4 / GeForce RTX 4060 8GB / NVME 480GB", 
+        preco: 7000.00, 
+        descricao:"Acompanha: Cabo de força, caixas, manuais e acessórios das peças utilizadas na montagem. Todas as máquinas vão com Windows 11 (TRIAL) InstaladoImagens do produto são meramente ilustrativas..",
+        imagemProduto: {
+            img1:"./images/products/prod02/img1.png",
+            img2:"./images/products/prod02/img2.png",
+            img3:"./images/products/prod02/img3.png",
+            img4:"./images/products/prod02/img4.png",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true
+    },
+
+    
+
+    /**MAIS VENDIDOS */
+
+    {
+        codigoProduto:9,
+        tituloProduto:"Galax Gaming ‎Chair GC-02", 
+        preco: 300.00, 
+        descricao:"Galax Gaming ‎Chair GC-02 Cadeira Gamer com grande.",
+        imagemProduto: {
+            img1:"./images/products/prod03/img2.png",
+            img2:"./images/products/prod03/img2.png",
+            img3:"./images/products/prod03/img3.png",
+            img4:"./images/products/prod03/img4.png",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:10,
+        tituloProduto:"Cadeira Gamer Profissional com Apoio para Pés MVP Vermelha e preto", 
+        preco: 1000.90, 
+        descricao:"Cadeira Gmaer Profissional Com desing Vermelho e preto.",
+        imagemProduto: {
+            img1:"./images/products/prod03/img4.png",
+            img2:"./images/products/prod03/img2.png",
+            img3:"./images/products/prod03/img3.png",
+            img4:"./images/products/prod03/img4.png",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:11,
+        tituloProduto:"Cadeira Gamer Profissional TGC12 Preta/Verde ThunderX3", 
+        preco: 1000.00, 
+        descricao:" .",
+        imagemProduto: {
+            img1:"./images/products/prod03/img4.png",
+            img2:"./images/products/prod03/img2.png",
+            img3:"./images/products/prod03/img3.png",
+            img4:"./images/products/prod03/img4.png",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+
+    {
+        codigoProduto:12,
+        tituloProduto:"Cadeira gamer Profissional Gamer", 
+        preco: 700.00, 
+        descricao:".",
+        imagemProduto: {
+            img1:"./images/products/prod03/img3.png",
+            img2:"./images/products/prod03/img2.png",
+            img3:"./images/products/prod03/img3.png",
+            img4:"./images/products/prod03/img4.png",
+
+        },
+        categoriaProduto: 'Ferramentas',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+   
+]
